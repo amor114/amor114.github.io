c7c3efd3ff30c82371adb2a75dddcf5d90883c65
amor114 <morrisali671@gmail.com>
2026-04-07 17:16:42 -0500

    Update weather.js

--- weather/weather.js
+++ weather/weather.js
@@ -17,7 +17,7 @@ $.getJSON(weatherAlertsUrl, function(data) {
      return { color: alertColor };
      if (feature.properties.severity === 'Severe') alertColor = 'red';
      return { color: alertColor };
-     if (feature.properties.severity === 'Minor') alertColor = 'yellow';
+     if (feature.properties.severity === 'Moderate') alertColor = 'yellow';
      return { color: alertColor };
     },
     onEachFeature: function(feature, layer) {
